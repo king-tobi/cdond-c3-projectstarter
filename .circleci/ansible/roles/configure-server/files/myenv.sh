7298e6ee16490d9b947147cba542dd83fcd4021f
king-tobi <ojelade.oluwadaniel@gmail.com>
2023-01-26 20:26:06 +0100

    configure infrastructure

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,9 @@
+
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_HOST=database-1.cnanchenrhf1.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432 
+export TYPEORM_USERNAME=posgres 
+export TYPEORM_PASSWORD=postgres
